2420356d234de657a4ab241978283b127d189ab1
Binita rijal <rijalbinita01@gmail.com>
2025-12-27 17:20:15 +0545

    all four working apis created

--- src/controllers/category.controllers.js
+++ src/controllers/category.controllers.js
@@ -81,6 +81,43 @@ catch(error){
 }
 
 }
-module.exports = {
-  createCategory, showCategory,updateCategory
+const deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const category = await Category.findById(id);
+    if (!category) {
+      return res.status(404).json({
+        message: "Category not found",
+      });
+    }
+
+    if (category.logo) {
+      const imagePath = path.join(__dirname, "..", category.logo);
+      if (fs.existsSync(imagePath)) {
+        fs.unlinkSync(imagePath);
+      }
+    }
+
+    await Category.findByIdAndDelete(id);
+
+    res.status(200).json({
+      message: "Category deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
+
+
+
+
+
+module.exports = {
+  createCategory,
+   showCategory,
+   updateCategory,
+    deleteCategory
+}
--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,10 +1,11 @@
 const express= require('express')
 const router= express.Router();
 const upload= require("../middlewares/upload")
-const {createCategory, showCategory, updateCategory}= require("../controllers/category.controllers")
+const {createCategory, showCategory, updateCategory, deleteCategory}= require("../controllers/category.controllers")
 
 router.post("/",upload.single("logo"),createCategory)
 router.get("/",showCategory)
 router.put("/:id",upload.single("logo"),updateCategory)
+router.delete("/:id",upload.single("logo"),deleteCategory)
 
 module.exports=router;
